37d025053ac328b4d27cf145e9796b331b44dc0b
Selina Ali <selinaali@Selinas-MacBook-Pro.local>
2021-03-29 13:49:03 -0400

    started weekly testing

--- weeklyTesting.py
+++ weeklyTesting.py
@@ -5,7 +5,6 @@ def region_testing_loop():
     while True:
         region_menu()
         choice = int(input("Enter your choice [1-6]: "))
-
         if choice >= 1 and choice <=5:
             cursor.execute("select sum(DailyTested) from WeeklyTesting where WeeklyTesting.region=%s", (choice,)) 
             print("There were", cursor.fetchone()[0], "COVID-19 tests conducted in the", region_options[choice-1], "region." )
@@ -19,7 +18,6 @@ def timeline_testing_loop():
     while True:
         timeline_menu()
         choice = int(input("Enter your choice [1-9]: "))
-
         if choice>=1 and choice <= 8:
             cursor.execute("SELECT sum(DailyTested) FROM WeeklyTesting WHERE WeeklyTesting.episodeWeek=%s", (choice+35,)) 
             print("For", timeline_options[choice-1], "there were", cursor.fetchone()[0], "confirmed COVID-19 conducted.")
