b21198912112c53a14a871fb48a38edb1a7ea4f0
Matthew Tang <matthewtang1999@gmail.com>
2021-04-14 22:33:20 -0400

    Data Mining Done

--- Data_Mining/dataMining.py
+++ Data_Mining/dataMining.py
@@ -3,9 +3,14 @@ import graphviz
 import pickle
 from sklearn import tree
 from sklearn.datasets import load_iris
+from sklearn.model_selection import KFold
+from sklearn.utils import shuffle
+import numpy as np
+
 
 fields = ['Region', 'Episode week', 'Gender', 'Age group', 'Occupation']
 fields2 = ['Died'] # Survived = 0, Died = 1
+SEED = 2
 
 def read_data():
     print("Reading Data . . .")
@@ -13,15 +18,31 @@ def read_data():
     df2 = pd.read_csv('Data_Mining/dataMiningData.csv', skipinitialspace=True, usecols=fields2)
 
     # User list comprehension to create a list of lists from Dataframe rows
-    data = [list(row) for row in df.values]
-    outcome = [list(row) for row in df2.values]
+    data = np.array([list(row) for row in df.values])
+    outcome = np.array([list(row) for row in df2.values])
 
     print("Creating Classification Tree. . .")
-    clf = tree.DecisionTreeClassifier()
-    clf = clf.fit(data, outcome)
+    kf = KFold(n_splits=10, random_state=SEED, shuffle=True)
+    max_accuracy = 0
+    best_clf = None
+    
+    for train_index, test_index in kf.split(data):
+        X_train, X_test = data[train_index], data[test_index]
+        y_train, y_test = outcome[train_index], outcome[test_index]
 
+        clf = tree.DecisionTreeClassifier()
+        clf = clf.fit(X_train, y_train)
+
+        accuracy = clf.score(X_test, y_test)
+        # print('New accuracy: ', accuracy)
+
+        if accuracy > max_accuracy:            
+            best_clf = clf 
+            max_accuracy = accuracy
+
+    # print('Highest Value Accuracy: ', max_accuracy)
     # Save the classifer so that it can be reused
-    pickle.dump(clf, open("classifier.pickle", "wb"))
+    pickle.dump(best_clf, open("classifier.pickle", "wb"))
     print("Data Mining Done!")
 
 def data_mine(input_data):    
